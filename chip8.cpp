30f2bf73608958e47410819afa9812b9c6f24cb8
Martin <mthingvold@gmail.com>
2021-06-02 18:12:59 -0600

    Implemented the run function

--- chip8.cpp
+++ chip8.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
 #include <map>
 
 class chip8 {
@@ -12,6 +14,21 @@ class chip8 {
     uint8_t delay_timer;
     uint8_t sound_timer;
     vector<uint16_t> stack;
+    uint8_t memory[4096];
+    bool display_ram[32][64];
+    map<uint8_t, SDL_KeyCode> keymap;
+
+    void run(){
+        for(;;) {
+            if (delay_timer > 0){ delay_timer--;}
+            if (sound_timer > 0){ sound_timer--;}
+            uint16_t instruction = next_instruction();
+        }
+    }
+
+    uint16_t next_instruction(){
+        return 0;
+    }
 };
 
 int main(){
