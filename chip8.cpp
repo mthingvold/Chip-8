dea6af2f618bd02fc38a788966b3579b5161123d
Martin <mthingvold@gmail.com>
2021-06-03 15:26:08 -0600

    Added jump/ret/call. Implemented next_instruction

--- chip8.cpp
+++ chip8.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <chrono>
+#include <thread>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 #include <map>
+using namespace std;
 
 class chip8 {
     public:
@@ -23,12 +25,40 @@ class chip8 {
             if (delay_timer > 0){ delay_timer--;}
             if (sound_timer > 0){ sound_timer--;}
             uint16_t instruction = next_instruction();
+            exec(instruction);
+            this_thread::sleep_for(chrono::milliseconds(2));
+        
         }
     }
 
     uint16_t next_instruction(){
         return 0;
     }
+
+    void exec(uint16_t instruction){
+        int l = (instruction & 0xF000) >> 12;
+        int x = (instruction & 0x0F00) >> 8;
+        int y = (instruction & 0x00F0) >> 4;
+
+        int nnn = instruction & 0x0FFF;
+        int nn = instruction & 0x00FF;
+        int n = instruction & 0x000F;
+    }
+    private:
+    void jump(uint16_t instruction){
+        pc = instruction;
+    }
+
+    void call(uint16_t instruction){
+        stack.push_back(pc);
+        jump(instruction);
+    }
+
+    void ret(){
+        pc = stack.back();
+        stack.pop_back();
+    }
+ 
 };
 
 int main(){
